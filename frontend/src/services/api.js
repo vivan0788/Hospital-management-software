83a76f31bf7e88203f58662c116678454164ab0d
Avinash Sahani <232133417+vivan0788@users.noreply.github.com>
2026-07-03 18:46:05 +0530

    Add API service with axios and interceptors

---
+++ frontend/src/services/api.js
@@ -0,0 +1,21 @@
+import axios from 'axios';
+
+const API = axios.create({
+  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:5000/api',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+// Interceptor to attach Authorization tokens automatically
+API.interceptors.request.use((config) => {
+  const token = localStorage.getItem('token');
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+}, (error) => {
+  return Promise.reject(error);
+});
+
+export default API;
